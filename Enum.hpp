685b5d3592010b736998341290371f80d06dfbe0
N1ckl <145074533+N1ckl@users.noreply.github.com>
2024-02-10 16:49:58 -0500

    Update Enum.hpp

--- Enum.hpp
+++ Enum.hpp
@@ -1,15 +1,10 @@
-
 #ifndef ENUMS_H
 #define ENUMS_H
-#include "tools.hpp"
 
 
-enum class ECcolor {
-    white,orange,yellow,green,blue,red
-    /*,red Red is the Error color */
+enum class ECcolor { 
+white,orange,yellow,green, blue //,red
+// Red is the Error color
 };
-const string colorStrings[] = { "White", "Orange", "Yellow", "Green", "Blue", "Red"};
-ECcolor inputColor();
-string colorToString(ECcolor color);
-
-#endif
+  extern const char* const ECcolorStrings[];
+  #endif
